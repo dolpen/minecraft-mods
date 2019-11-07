9c7ba0a1585baf8a96b8784e3bd1f2666beaaf1c
dolpen <dolpen@dolpen.net>
2019-11-07 22:22:21 +0900

    mod info

--- src/main/java/net/dolpen/mcmod/ext/settings/Constants.java
+++ src/main/java/net/dolpen/mcmod/ext/settings/Constants.java
@@ -6,15 +6,11 @@ public class Constants {
 
     public static final String MOD_ID = "dolpenext";
     public static final String MOD_NAME = "Dolpen Extras";
-    // ツール定義
-    public static final String TOOL_CUT = "axe";
-    public static final String TOOL_DIG = "shovel";
-    public static final String TOOL_MINE = "pickaxe";
+    public static final String MOD_VERSION = "0.0.2";
     public static final ResourceLocation CAPABILITY_PLAYER = new ResourceLocation(
             MOD_ID,
             String.join(".", "capability", "player")
     );
-    public static final String MOD_VERSION = "0.1.0";
     private static final String KEY_LABEL_PREFIX = MOD_ID + "." + "key";
     public static final String KEY_LABEL_TOGGLE = KEY_LABEL_PREFIX + "." + "toggle";
     private static final String MESSAGE_LABEL_PREFIX = MOD_ID + "." + "message";
